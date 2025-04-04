850336e8ad3d30327301178e2aee1074775f3708
Kaden Nguyen <94870428+kadennguyen0329@users.noreply.github.com>
2025-04-04 04:54:06 +0000

    hi

--- python/db.py
+++ python/db.py
@@ -1,10 +1,11 @@
 from pymongo import MongoClient, errors
 from bson.json_util import dumps
+from urllib.parse import quote_plus
 import os
 
 # mongohost should look something like 'mongodb+srv://cluster0.xxxxx.mongodb.net/'
-MONGOUSER = os.getenv('MONGOUSER')
-MONGOPASS = os.getenv('MONGOPASS')
+MONGOUSER = quote_plus(os.getenv('MONGOUSER'))
+MONGOPASS = quote_plus(os.getenv('MONGOPASS'))
 MONGOHOST = os.getenv('MONGOHOST')
 client = MongoClient(MONGOHOST, username=MONGOUSER, password=MONGOPASS, connectTimeoutMS=200, retryWrites=True)
 sampler = client.sample_restaurants
